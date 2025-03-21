c40c7bea6c7f3801bd8b76e96e3ddc4d6b10f4a5
vineethannur <137258106+vineethannur@users.noreply.github.com>
2025-03-21 15:39:38 +0530

    Add files via upload

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
+    include: ['jspdf', 'html2canvas'],
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['lucide-react'],
+    }
   },
   server: {
     proxy: {
